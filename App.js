eb513a8e78c2e7f22f79ec8f7de485fa73954a3c
SehajbirBrar <120230498+SehajbirBrar@users.noreply.github.com>
2023-11-20 14:18:07 -0700

    Lab5

--- App.js
+++ App.js
@@ -3,23 +3,34 @@
  *
  * @format
  */
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import React, { useState } from 'react';
 import {View,} from 'react-native';
-import ToDoList from './ToDoList';
-import ToDoForm from './ToDoForm';
+import ToDoList from './components/ToDoList';
+import ToDoForm from './components/ToDoForm';
 
 export default function App() {
+    const Stack = createStackNavigator();
     const [tasks,setTasks]=useState([
         'Do laundry',
         'Go to gym',
         'Walk dog'
-      ])
+      ]);
+    const addTask = (task) => {
+        setTasks([...tasks, task]);
+    };
     return (
+        <NavigationContainer>
         <View>
         <ToDoList tasks={tasks}/>
-        <ToDoForm />
+        <ToDoForm addTask={addTask} />
+        <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
+        </Stack.Navigator>
         </View>
+        </NavigationContainer>
     );
 }
 
